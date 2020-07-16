8792543066ef426e56189d058a3c28acf8d833df
Lilith <lilith@idsia.ch>
2020-07-16 19:29:58 +0200

    lil

--- examples/marginalization.jl
+++ examples/marginalization.jl
@@ -40,136 +40,156 @@ using ProbabilisticCircuits
 
 
 
-dataSet=dataset(twenty_datasets("nltcs"); do_shuffle=false, batch_size=-1)
-#only_shuffle_split_train_valid_test(dataSet, percs=[0.75, 0.1, 0.15], rand_gen=None)
-dataTrain = dataSet.train
+#  dataSet=dataset(twenty_datasets("nltcs"); do_shuffle=false, batch_size=-1)
+#  dataTrain = dataSet.train
+#  l = num_examples(dataTrain)
+#  println("num examples = ", l)
 
-println("type of dataTrain = ", typeof(dataTrain)) #type of dataTrain = PlainXData{Bool,BitArray{2}}
 
+# println("type of dataTrain = ", typeof(dataTrain)) #type of dataTrain = PlainXData{Bool,BitArray{2}}
 
-matrixTrain=convert.(Int8,dataSet.train.x) #covert Bool to Int8
-matrixTrain2 = matrixTrain[1:100,:]
 
+# matrixTrain=convert.(Int8,dataSet.train.x) #covert Bool to Int8
+# matrixTrain2 = matrixTrain[1:100,:]
 
-#println(matrixTrain2)
 
-#dataTrain2 = PlainXData(matrixTrain2) NO MI RITORNA PlainXData{Int8,Array{Int8,2}} e io voglio PlainXData{Bool,BitArray{2}}
+# dataTrain2_ = convert.(Bool,matrixTrain2)
 
-dataTrain2_ = convert.(Bool,matrixTrain2)
+ #dataTrain2 = PlainXData(dataTrain2_)
 
-dataTrain2 = PlainXData(dataTrain2_)
+# println(typeof(dataTrain2))
 
-println(typeof(dataTrain2))
 
+# # data validation
 
-# data validation
+# matrixValid = convert.(Int8,dataSet.valid.x) #covert Bool to Int8
 
-matrixValid = convert.(Int8,dataSet.valid.x) #covert Bool to Int8
+#dataValid = XData(matrixValid) 
 
-dataValid = XData(matrixValid) 
 
 
 
+# #println("type Datatest",typeof(dataTest))
 
-#println("type Datatest",typeof(dataTest))
+#  #csdd = learn_credal_circuit(WXData(dataTrain2), 40.0); #using clt
 
- #csdd = learn_credal_circuit(WXData(dataTrain2), 40.0); #using clt
+#  csdd = learn_credal_circuit(WXData(dataTrain), 40.0); #using clt
 
- csdd = learn_credal_circuit(WXData(dataTrain), 40.0); #using clt
 
 
 
+ #println(twenty_dataset_names)
 
+   ###############################################################################################################################################
+#   # LLL, LLU and gap for growing portions of training data, for the twenty datasets in https://github.com/UCLA-StarAI/Density-Estimation-Datasets
+#   ###############################################################################################################################################
+   
+    function LL_lo_up_twenty_ds(training_portion::Float64)
+  
+       show = Array{Tuple{String,Float64,Float64,Float64}}(undef,21)
 
+       for i=1:21 #name in twenty_dataset_names
+          name = twenty_dataset_names[i]
+          
+          dataSet=dataset(twenty_datasets(name); do_shuffle=false, batch_size=-1)
+          dataTrain = dataSet.train
+          l = num_examples(dataTrain)
 
- ########################################################################################
- # LLL, LLU and gap for growing portions of training data 
- ########################################################################################
 
+          matrixValid = convert.(Int8,dataSet.valid.x) #covert Bool to Int8
+          dataValid = XData(matrixValid) 
 
- for i=1:16
 
-    matrixTrain_i = matrixTrain[1:1000*i,:]
-    dataTrain_i = PlainXData(convert.(Bool,matrixTrain_i))
 
-    csdd = learn_credal_circuit(WXData(dataTrain_i), 40.0); #using clt
+          matrixTrain_ = convert.(Int8, dataSet.train.x)
+          matrixTrain = matrixTrain_[1:trunc(Int, training_portion*l),:]
 
-    LLL_i = sum(log_marginal_lower(csdd, dataValid))
-    LLU_i = sum(log_marginal_upper(csdd, dataValid))
+          dataTrain= PlainXData(convert.(Bool,matrixTrain))
 
+          csdd = learn_credal_circuit(WXData(dataTrain), 40.0); #using clt
 
-    println("LLL of data validation = ", LLL_i)
-    println("LLU of data validation = ", LLU_i)
-    println("LL gap = ", LLL_i - LLU_i)
+          LLL = sum(log_marginal_lower(csdd, dataValid))
+          LLU = sum(log_marginal_upper(csdd, dataValid))
+          gap = - (LLL-LLU)
 
- end
+          show[i] = (name, LLL, LLU, gap)
 
+          end
 
- ########################################################################################
 
+       return show
 
+    end
 
-# obs1 = XData(Int8.([1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1;0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1;0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1])) 
+    println( LL_lo_up_twenty_ds(0.16181))
+  
 
 
+# #########################################################################################
 
 
-# upper_marg_obs1 = exp.(log_marginal_upper(csdd, obs1))
-# lower_marg_obs1 = exp.(log_marginal_lower(csdd, obs1))
 
+# # obs1 = XData(Int8.([1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1;0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1;0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1])) 
 
-# println("Upper marginal obs1: $(upper_marg_obs1)")
 
-# println("Lower marginal obs1: $(lower_marg_obs1)")
 
 
+# # upper_marg_obs1 = exp.(log_marginal_upper(csdd, obs1))
+# # lower_marg_obs1 = exp.(log_marginal_lower(csdd, obs1))
 
-# # Testing log_likelihood of test dataset
 
-# LLL = sum(log_marginal_lower(csdd, dataValid))
-# LLU = sum(log_marginal_upper(csdd, dataValid))
+# # println("Upper marginal obs1: $(upper_marg_obs1)")
 
+# # println("Lower marginal obs1: $(lower_marg_obs1)")
 
-# println("LLL of data validation = ", LLL)
-# println("LLU of data validation = ", LLU)
 
 
+# # # Testing log_likelihood of test dataset
 
+# # LLL = sum(log_marginal_lower(csdd, dataValid))
+# # LLU = sum(log_marginal_upper(csdd, dataValid))
 
-# # Testing complete evidence likelihood upper and lower flows
 
-# complete_obs = XData(Bool.([1 1 1 0 0 1 1 1 1 1 1 1 1 1 1 1; 1 1 1 0 0 1 1 1 1 1 0 0 0 0 0 0]))
+# # println("LLL of data validation = ", LLL)
+# # println("LLU of data validation = ", LLU)
 
 
 
-# upper_marg_complete_obs = exp.(log_marginal_upper(csdd, obs1))
-# lower_marg_complete_obs = exp.(log_marginal_lower(csdd, obs1))
 
+# # # Testing complete evidence likelihood upper and lower flows
 
-# println("Upper of complete obs using marginal: $(upper_marg_complete_obs)")
+# # complete_obs = XData(Bool.([1 1 1 0 0 1 1 1 1 1 1 1 1 1 1 1; 1 1 1 0 0 1 1 1 1 1 0 0 0 0 0 0]))
 
-# println("Lower of complete obs using marginal: $(lower_marg_complete_obs)")
 
 
-# # lower_prob = exp.(log_prob_lower(csdd, complete_obs))
-# # upper_prob = exp.(log_prob_upper(csdd, complete_obs))
-# # println("Lower prob: $(lower_prob)")
-# # println("Upper prob: $(upper_prob)")
+# # upper_marg_complete_obs = exp.(log_marginal_upper(csdd, obs1))
+# # lower_marg_complete_obs = exp.(log_marginal_lower(csdd, obs1))
 
 
+# # println("Upper of complete obs using marginal: $(upper_marg_complete_obs)")
 
+# # println("Lower of complete obs using marginal: $(lower_marg_complete_obs)")
 
-# # Testing conditional inference upper and lower bounds
-# ## 1,0 observed/evidence variables values; 2,3 query varables values for 0,1;  -1 marginalize variables.
-# ## cond_queries:    X_1=0,X_2=0|X_3=0,X_4=0
-# ##             X_15=1,X_16=1|X_1=1,X_2=1             
-# ######cond_queries= XData(Int8.([2 1 0 0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1; 1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 3 1; 1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 3 1 ]))
-# ######lower_cond = conditional_lower(csdd, cond_queries, [0.5, 0.6, 0.7])
-# #upper_cond = conditional_upper(csdd, cond_queries)
-# ######println("Lower cond. prob: $(lower_cond)")
-# #println("Upper cond. prob: $(upper_cond)")
 
-# # for n in csdd
-# #     println("type of node : ", typeof(n))
-# # end
+# # # lower_prob = exp.(log_prob_lower(csdd, complete_obs))
+# # # upper_prob = exp.(log_prob_upper(csdd, complete_obs))
+# # # println("Lower prob: $(lower_prob)")
+# # # println("Upper prob: $(upper_prob)")
+
+
+
+
+# # # Testing conditional inference upper and lower bounds
+# # ## 1,0 observed/evidence variables values; 2,3 query varables values for 0,1;  -1 marginalize variables.
+# # ## cond_queries:    X_1=0,X_2=0|X_3=0,X_4=0
+# # ##             X_15=1,X_16=1|X_1=1,X_2=1             
+# # ######cond_queries= XData(Int8.([2 1 0 0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1; 1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 3 1; 1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 3 1 ]))
+# # ######lower_cond = conditional_lower(csdd, cond_queries, [0.5, 0.6, 0.7])
+# # #upper_cond = conditional_upper(csdd, cond_queries)
+# # ######println("Lower cond. prob: $(lower_cond)")
+# # #println("Upper cond. prob: $(upper_cond)")
+
+# # # for n in csdd
+# # #     println("type of node : ", typeof(n))
+# # # end
 
